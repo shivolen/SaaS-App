07dd85aca4c07f461b69ca7c414140b7b24e7a2f
Shivolen <mshivolen@gmail.com>
2025-11-16 12:07:38 +0530

    updated the clerk auth page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,18 @@
+import CompanionForm from "@/components/CompanionForm";
 
 
 const page = () => {
   return (
-    <div>Companion Builder</div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+        <article className='w-full gap-4'>
+            <h1>Companion Builder</h1>
+
+            <CompanionForm/>
+        </article>
+    </main>
   )
 }
 
 export default page
+
+
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 const Navbar = () => {
   return (
     <nav className='navbar'>
@@ -17,8 +23,14 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center gap-8">
             <NavItems />
-            <p>Sign In</p>
-
+            <SignedOut>
+              <SignInButton>
+                <button className='btn-signin'>Sign In</button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
